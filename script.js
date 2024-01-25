ce6601ab2b29d28570ce134a7e0216a6fbc38b9a
vercingatorix1 <67870559+vercingatorix1@users.noreply.github.com>
2024-01-25 11:48:45 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -203,33 +203,64 @@ function handleAddUserStoryFormSubmission(event) {
 function getNextWorkingDay(date) {
   const dayOfWeek = date.getDay();
 
-  if (dayOfWeek === 0 || dayOfWeek === 6) {
-    date.setDate(date.getDate() + (8 - dayOfWeek));
+  if (dayOfWeek === 0) {
+    return "Sunday";
+  } else if (dayOfWeek === 6) {
+    return "Saturday";
   }
 
   return date;
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  generateFeatureNameOptions();
-  generateDependsOnOptions();
-});
+function countWeekendDays() {
+  const today = new Date();
+  let count = 0;
+
+  for (let i = 0; i < 10; i++) {
+    const nextWorkingDay = getNextWorkingDay(new Date(today.getTime() + i * 24 * 60 * 60 * 1000));
+    
+    if (nextWorkingDay === "Saturday" || nextWorkingDay === "Sunday") {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+const weekendDayCount = countWeekendDays();
+console.log("Number of weekend days in the next 10 working days:", weekendDayCount);
+
 
 function addDates() {
   const datesContainer = document.getElementById('datesContainer');
   const today = new Date();
+  const count = countWeekendDays(); // Calculate the weekend day count
 
-  for (let i = 9; i >= 0; i--) {
-    const dateElement = document.createElement('div');
-    dateElement.className = 'date';
-    dateElement.style.height = `100px`;
-    dateElement.innerText = getNextWorkingDay(new Date(today.getTime() + i * 24 * 60 * 60 * 1000))
-      .toLocaleDateString('en-US', { month: 'short', day: 'numeric' });
-    datesContainer.appendChild(dateElement);
+
+  for (let i = 10 + count; i >= 0; i--) {
+    const nextWorkingDay = getNextWorkingDay(new Date(today.getTime() + i * 24 * 60 * 60 * 1000));
+    
+    if (nextWorkingDay !== "Sunday" && nextWorkingDay !== "Saturday") {
+      const dateElement = document.createElement('div');
+      dateElement.className = 'date';
+      dateElement.style.height = `100px`;
+      
+      const dateText = nextWorkingDay.toLocaleDateString('en-US', { month: 'short', day: 'numeric' });
+      dateElement.innerText = dateText;
+      datesContainer.appendChild(dateElement);
+    }
   }
 }
 
-addDates();
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  generateFeatureNameOptions();
+  generateDependsOnOptions();
+  addDates();
+});
 
 document.getElementById('addStoryButton').addEventListener('click', showAddUserStoryForm);
 document.getElementById('addFeatureButton').addEventListener('click', showAddFeatureForm);
